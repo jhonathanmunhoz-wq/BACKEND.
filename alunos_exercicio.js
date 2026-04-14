531603f0c49de95dedf244ffedb92f0aabc238ac
jhonathanmunhoz-wq <jhonathan.munhoz@escola.pr.gov.br>
2026-04-14 09:11:38 -0300

    Add files via upload

---
+++ alunos_exercicio.js
@@ -0,0 +1,40 @@
+// =============== ATIVIDADES COM FOR ================ //
+
+
+for(let i=1; i<=10; i++){
+    console.log(i);}
+
+
+// 2 Exiba todos os numeros pares de 2 20
+for(let i=10; i>=1; i--){
+    console.log(i);}
+
+
+// 3: Exiba todos os numeros pares de 2 a 20
+console.log("\n==== numeros pares até 20 ====")
+for(let i=2; i<=20; i= i + 2) {
+    console.log(i);}
+
+
+// 4: Exiba a tabuada do numero 5 (5x1 ate 5x10);
+console.log("\n=== tabuada do 5 ====")
+for(let i=1; i<=10; i++){
+    console.log(`5 x ${i} = ${5*i}`);
+
+}
+
+// 5: calcule e exiba a soma dos numeros de 1 ate 50
+console.log("\n=== Soma de 1 a 50 =====");
+for(let i =1; i<=50; i++){
+    soma = soma + i;}
+    console.log(`A soma é: ${soma}`);
+
+
+// 6: conte de 1 até o numero 8 (use for):
+for(let i=1; i<=8; i++){
+    console.log(i);}
+
+
+// 7: 
+for(let i= 1; i<=15; i= 1 +2){
+    console.log(i);}
